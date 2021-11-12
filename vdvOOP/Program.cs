2f8f83313753bf810fffeafdd410551e3029c7a5
thanh2k1582 <71741876+thanh2k1582@users.noreply.github.com>
2021-11-12 23:42:39 +0700

    thanhnguyen

---
+++ vdvOOP/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace vdvOOP
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VanDongVien vdv1 = new VanDongVien("thanhnguyen",18,"lol",69,165);
+            vdv1.Xuat();
+            int n;
+            Console.WriteLine("Nhap vao so van dong vien: ");
+            n = Int32.Parse(Console.ReadLine());
+
+            ArrayList danhsach = new ArrayList(n);
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap ten van dong vien: ");
+                string hoten = Convert.ToString(Console.ReadLine());
+
+                Console.Write("Nhap tuoi van dong vien: ");
+                int tuoi = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Nhap mon thi dau cua van dong vien: ");
+                string monthidau = Convert.ToString(Console.ReadLine());
+
+                Console.Write("Nhap can nang: ");
+                double cannang = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Nhap chieu cao: ");
+                double  chieucao = Convert.ToDouble(Console.ReadLine());
+                danhsach.Add(new VanDongVien(hoten, tuoi,monthidau,cannang, chieucao));
+                Console.WriteLine("");
+
+            }
+            foreach (var item in danhsach)
+            {
+                Console.WriteLine(item);
+                if()
+            }
+            danhsach.Sort();
+            Console.WriteLine("\n\ndanh sach in ra la: \n\n");
+            foreach (VanDongVien item in danhsach)
+            {
+                item.Xuat();
+            }
+            /*ArrayList arlist = new ArrayList();
+            arlist.Add(300);
+            arlist.Add(200);
+            arlist.Add(100);
+            arlist.Add(400);
+            foreach (var item in arlist)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("");
+            arlist.Sort();
+            foreach (var item in arlist)
+            {
+                Console.WriteLine(item);
+            }*/
+
+
+        }
+    }
+}
---
+++ vdvOOP/VanDongVien.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vdvOOP
+{
+    class VanDongVien
+    {
+        private string v1;
+        private int v2;
+
+        public VanDongVien(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+        ~VanDongVien() { }
+        public string hoten { get; set; }
+        public int tuoi { get; set; }
+        public string monthidau { get; set; }
+        public double cannang { get; set; }
+        public double chieucao { get; set; }
+        public VanDongVien()
+        {
+            hoten = "thanhnguyen";
+            tuoi = 21;
+            monthidau = "lol";
+            cannang = 69;
+            chieucao = 165;
+        }
+        public VanDongVien(string ht , int t , string mtd , double cn , double cc)
+        {
+            hoten = ht;
+            tuoi = t;
+            monthidau = mtd;
+            cannang = cn;
+            chieucao = cc;
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Van dong vien co: ");
+            Console.WriteLine("Ten la {0}",hoten);
+            Console.WriteLine("Tuoi la {0}",tuoi);
+            Console.WriteLine("Thi dau bo mon {0}",monthidau);
+            Console.WriteLine("Can nang : {0}",cannang);
+            Console.WriteLine("Chieu cao : {0}",chieucao);
+            Console.WriteLine("");
+        }
+        public int Sosanh2(VanDongVien vdv1) {
+            VanDongVien temp;
+            if (chieucao > vdv1.chieucao)
+                return 1;
+            else if (chieucao < vdv1.chieucao)
+                return 0;
+            else if (cannang > vdv1.cannang)
+                return 1;
+            else
+                return 0;
+        }
+        public void swap(int n)
+        {
+            for(int i =0; i < n - 1; i++)
+            {
+                for(int j = 0; j < n - i - 1; j++)
+                {
+                    VanDongVien i;
+                    if()
+                }
+            }
+        }
+        
+
+
+
+    }
+}
